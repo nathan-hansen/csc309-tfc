e83bdfcc21927ac6b31a317aa245f7d6952e8025
Nathan Hansen <nathanajhansen@gmail.com>
2022-11-10 16:52:37 -0500

    resolved initial model errors

--- classes/models.py
+++ classes/models.py
@@ -1,10 +1,10 @@
 from django.db import models as m
 
 class Class(m.Model):
-    studio = m.ForeignKey('Studio', on_delete=m.SET_NULL, related_name='studio')
-    name = m.CharField()
+    studio = m.ForeignKey('studios.Studio', on_delete=m.CASCADE, related_name='tfc_class')
+    name = m.CharField(max_length=250)
     description = m.TextField()
-    coach = m.CharField()
+    coach = m.CharField(max_length=250)
     keywords = m.TextField()
     capacity = m.IntegerField()
     start_time = m.DateTimeField()
--- studios/models.py
+++ studios/models.py
@@ -2,13 +2,13 @@ from django.db import models as m
 from django.contrib.gis.db import models as gm
 
 class Studio(m.Model):
-    name = m.CharField()
-    address = m.CharField()
+    name = m.CharField(max_length=250)
+    address = m.CharField(max_length=250)
     location = gm.PointField()
-    postal_code = m.CharField()
+    postal_code = m.CharField(max_length=250)
     phone_number = m.IntegerField()
 
 
 class StudioImage(m.Model):
-    studio = m.ForeignKey(Studio, on_delete=m.SET_NULL, related_name='studio')
+    studio = m.ForeignKey('Studio', on_delete=m.CASCADE, related_name='image')
     image = m.ImageField()
