2ac7df98e07f90ecf5bb6e4d05733adfe3521c06
RealFakeAccount <63186767+RealFakeAccount@users.noreply.github.com>
2022-11-12 17:32:09 -0500

    migrate

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,6 @@ class Account(User):
     # first/last name and email are inherited from User
     avatar = m.ImageField()
     phone_number = m.CharField(max_length=250)
-    current_subscription = m.ForeignKey('subscriptions.CurrentSubscription', on_delete=m.SET_NULL, related_name='account')
 
 class CurrentSubscription(m.Model):
     account = m.ForeignKey('Account', on_delete=m.CASCADE, related_name='current_subscription')
--- payments/models.py
+++ payments/models.py
@@ -2,14 +2,14 @@ from django.db import models as m
 
 
 class PaymentInfo(m.Model):
-    account = m.ForeignKey('Account', on_delete=m.CASCADE, related_name='payment_info')
+    account = m.ForeignKey('accounts.Account', on_delete=m.CASCADE, related_name='payment_info')
     card_number = m.CharField(max_length=250)
     expiry_date = m.DateField()
     cvv = m.IntegerField()
     name_on_card = m.CharField(max_length=250)
 
 class PaymentHistory(m.Model):
-    account = m.ForeignKey('Account', on_delete=m.CASCADE, related_name='payment_history')
+    account = m.ForeignKey('accounts.Account', on_delete=m.CASCADE, related_name='payment_history')
     timestamp = m.DateTimeField()
     amount = m.FloatField()
     card_number = m.CharField(max_length=250)
