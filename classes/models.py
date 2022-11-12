af6dee3fed84637a65f0e63716a8d87324983756
RealFakeAccount <63186767+RealFakeAccount@users.noreply.github.com>
2022-11-12 16:44:21 -0500

    init commit

--- classes/models.py
+++ classes/models.py
@@ -1,5 +1,6 @@
 from django.db import models as m
 
+
 class Class(m.Model):
     studio = m.ForeignKey('studios.Studio', on_delete=m.CASCADE, related_name='tfc_class')
     name = m.CharField(max_length=250)
