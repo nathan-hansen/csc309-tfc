8cccadb5acfd1237ede9c057f35b6edf18ec69f3
ed29leung <edwardleung.ca@gmail.com>
2022-11-13 23:01:56 -0500

    Added basic registration, login, view and edit profile functionality

---
+++ accounts/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from accounts.models import EnrollClass
+from classes.models import ClassTimeTable
+from accounts.models import Account
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    # we use this serializer to serialize the user model in the browsable api (get its data and display it/allow
+    # editing it)
+    class Meta:
+        model = Account
+        fields = ["username", "first_name", "last_name", "email", "phone_number"]
+        read_only_fields = ["password"]
+        # not allowed to edit this attribute, so read only, from tutorial code
+
+
+class EnrollClassSerializer(serializers.ModelSerializer):
+    account = AccountSerializer()
+
+    # classtime = ClassTimeTableSerializer()
+
+    class Meta:
+        model = EnrollClass
+        fields = ['account', 'classtime']
+
+
+# based on the tutorial code
+class SignupSerializer(serializers.ModelSerializer):
+    # Want a signup form that asks for the user's username, firstname, lastname, email, phone_number
+    # and password, we would also like to have an additional field to confirm the password (password2)
+    password2 = serializers.CharField(label=_("Confirm password"), write_only=True, style={"input_type": "password"})
+
+    class Meta:
+        model = Account  # returns the User model that is active in this project
+        fields = ("username", "password", "password2", "first_name", "last_name", "email", "phone_number")
+
+        # Specify the input type for password in the sign up, already did for password 2
+        # we can also specify the write_only attribute to specify that a field should only be used for writing data
+        # and not for reading data
+        extra_kwargs = {
+            "password": {"write_only": True, "style": {"input_type": "password"}},
+        }
+
+    def create(self, validated_data: dict) -> Account:
+        # pop password 2
+        validated_data.pop('password2')
+        # return the created user, use the User create_user method to make the user
+        return Account.objects.create_user(**validated_data)
