1a20713860239d7bc3b5c5ae6de9537b634d58b4
Nathan Hansen <nathanajhansen@gmail.com>
2022-11-19 16:32:36 -0500

    add startup.sh

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python3.10 -m venv env
+source env/bin/activate
+python3 -m pip install -r requirements.txt --force-reinstall
+python3 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
+
