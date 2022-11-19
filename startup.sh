56c2287e8b0821612cf4183d4f15166e0933446d
Nathan Hansen <nathanajhansen@gmail.com>
2022-11-19 16:45:35 -0500

    startup working w/ migrations deleted

--- startup.sh
+++ startup.sh
@@ -3,7 +3,6 @@
 python3.10 -m venv env
 source env/bin/activate
 python3 -m pip install -r requirements.txt --force-reinstall
-python3 manage.py migrate
 python3 manage.py makemigrations
 python3 manage.py migrate
 
