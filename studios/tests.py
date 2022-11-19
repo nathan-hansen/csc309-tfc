d0e29f962de586c5a3e1fa38163fb26d895fb782
RealFakeAccount <63186767+RealFakeAccount@users.noreply.github.com>
2022-11-18 19:44:47 -0500

    add two test for studio

--- studios/tests.py
+++ studios/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+import random, geopy
+
+from studios.models import Studio, StudioImage, Amenities
 
 # Create your tests here.
+class StudioTest(TestCase):
+    studios = []
+
+    def create_user(self):
+        self.username = "test_admin"
+        self.password = User.objects.make_random_password()
+        user, created = User.objects.get_or_create(username=self.username)
+        user.set_password(self.password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save()
+        self.user = user
+
+    def setUp(self) -> None:
+        self.create_user()
+        self.studios = [
+            Studio.objects.create(
+                name="studio{}".format(i),
+                address="address{}".format(i),
+                latitude=random.uniform(-90, 90),
+                longitude=random.uniform(-180, 180),
+                postal_code="postal_code{}".format(i),
+                phone_number="1234567890",
+            )
+            for i in range(random.randint(0, 20))
+        ]
+
+    def test_list_studio_by_proximity(self):
+        c = Client()
+        c.login(username=self.username, password=self.password)
+        
+            
+        for i in range(20):
+            cood = (random.uniform(-90, 90), random.uniform(-180, 180))
+            response = c.get(
+                "/studios/list/{},{}".format(cood[0], cood[1])
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(response.data), len(self.studios))
+
+            distance_list = [
+                (geopy.distance.distance(
+                    (studio.latitude, studio.longitude), cood
+                ).km, studio)
+                for studio in self.studios
+            ]
+            distance_list.sort(key=lambda x: x[0])
+
+            # print(response.content)
+            for i in range(len(self.studios)):
+                self.assertEqual(response.data[i]["name"], distance_list[i][1].name)
+        
+
