885d235486edac98537d0970d34eaab4a879c5ae
Nathan Hansen <nathanajhansen@gmail.com>
2022-11-18 20:42:22 -0500

    studio search/filter tentatively working

--- studios/views.py
+++ studios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from geopy.distance import geodesic
 from rest_framework import generics
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from studios.serializers import (
         StudioSerializer, 
         StudioImageSerializer, 
@@ -11,11 +13,30 @@ from studios.models import Studio, StudioImage, Amenities
 # user authenticated views
 class ListStudioByProximityView(generics.ListAPIView):
     serializer_class = StudioSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['name', 'amenities__amenity_type', 'tfc_class__name', 'tfc_class__coach']
+    search_fields = ['name', 'amenities__amenity_type', 'tfc_class__name', 'tfc_class__coach']
     def get_queryset(self):
         lat = float(self.kwargs['latitude'])
         lon = float(self.kwargs['longitude'])
-        print(f'lat: {lat}, lon: {lon}')
-        return Studio.get_locations_nearby_coords(lat, lon)
+
+        queryset = Studio.get_locations_nearby_coords(lat, lon)
+
+        # search queries
+        # studio_name = self.request.query_params.get('studio_name')
+        # amenity = self.request.query_params.get('amenity')
+        # class_name = self.request.query_params.get('class_name')
+        # coach = self.request.query_params.get('coach')
+        # if studio_name is not None:
+        #     queryset = queryset.filter(name=studio_name)
+        # if amenity is not None:
+        #     # get amenity if exists
+        #     amenity_queryset = Amenities.objects.filter(amenity_type=amenity)
+
+
+
+
+        return queryset
 
 class StudioView(generics.RetrieveAPIView):
     serializer_class = StudioSerializer
